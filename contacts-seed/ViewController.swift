6119de76ffd6396210be93edab1a03b48758f4a7
Isis Naomi Ramirez <isisnaomi@gmail.com>
2019-03-15 15:33:56 -0600

    Initial Commit

---
+++ contacts-seed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contacts-seed
+//
+//  Created by Isis Naomi Ramirez on 3/15/19.
+//  Copyright © 2019 Isis Naomi Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
